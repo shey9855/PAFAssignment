452ce95abaedee4f5985c2928b914e3641b63321
shey9855 <shehaniwije1998@gmail.com>
2021-05-13 22:33:39 +0530

    Completed upto creating the servlet class

--- PAF_Assignment_2/src/com/ProjectAPI.java
+++ PAF_Assignment_2/src/com/ProjectAPI.java
@@ -1,6 +1,10 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -39,7 +43,16 @@ public class ProjectAPI extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		
+		String output = project.insertItem(
+				request.getParameter("ProjectCode"),
+				request.getParameter("ProjectName"),
+				request.getParameter("Description"),
+				request.getParameter("Budget"),
+				request.getParameter("Category"));
+		
+		response.getWriter().write(output); 
 	}
 
 	/**
@@ -47,13 +60,54 @@ public class ProjectAPI extends HttpServlet {
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		Map paras = getParasMap(request);
+		
+		 String output = project.updateItem(paras.get("ProjectId").toString(),
+				 							paras.get("ProjectCode").toString(),
+											paras.get("ProjectName").toString(),
+											paras.get("Description").toString(),
+											paras.get("Budget").toString(),
+											paras.get("Category").toString());
+		response.getWriter().write(output);
 	}
+
+	private Map getParasMap(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		
+		Map<String, String> map = new HashMap<String, String>();
+		try
+		 {
+			 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			 String queryString = scanner.hasNext() ?
+			 scanner.useDelimiter("\\A").next() : "";
+			 scanner.close();
+			 String[] params = queryString.split("&");
+			 for (String param : params)
+			 { 
+		
+				 String[] p = param.split("=");
+				 map.put(p[0], p[1]);
+			 }
+		 }
+		catch (Exception e)
+		 {
+		 }
+		return map;
+	}
+
+
 
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		Map paras = getParasMap(request);
+		
+		String output = project.deleteItem(paras.get("ProjectId").toString());
+		response.getWriter().write(output);
 	}
 
 }

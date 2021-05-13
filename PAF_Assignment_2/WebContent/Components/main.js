87263e6e95bf22ca1441a03cf32a57ced30eb668
shey9855 <shehaniwije1998@gmail.com>
2021-05-13 22:55:27 +0530

    create main.js class

---
+++ PAF_Assignment_2/WebContent/Components/main.js
@@ -0,0 +1,29 @@
+$(document).on("click", "#btnSave", function(event)
+{
+// Clear alerts---------------------
+ $("#alertSuccess").text("");
+ $("#alertSuccess").hide();
+ $("#alertError").text("");
+ $("#alertError").hide();
+// Form validation-------------------
+var status = validateItemForm();
+if (status != true)
+ {
+ $("#alertError").text(status);
+ $("#alertError").show();
+ return;
+ }
+// If valid------------------------
+var type = ($("#ProjectId").val() == "") ? "POST" : "PUT";
+ $.ajax(
+ {
+ url : "ProjectAPI",
+ type : type,
+ data : $("#formItem").serialize(),
+ dataType : "text",
+ complete : function(response, status)
+ {
+ onItemSaveComplete(response.responseText, status);
+ }
+ });
+});
